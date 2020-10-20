594c8f518ad222330b4f16fe6efc5364309238f3
Ludovico Forace <ludovicoforace@yahoo.com>
2020-10-21 00:29:16 +0100

    Updates

---
+++ precache-manifest.63a7d0a99edb8136806a52a5c45ea4b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27116a6a07d7e593d317183df2e27287",
+    "url": "/avatar-picker/index.html"
+  },
+  {
+    "revision": "5073b7b48d6ee43cf525",
+    "url": "/avatar-picker/static/js/2.672794c8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/avatar-picker/static/js/2.672794c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043c590de99c286a9b8e",
+    "url": "/avatar-picker/static/js/main.185eb93f.chunk.js"
+  },
+  {
+    "revision": "a24e5e160dbab8ee05f4",
+    "url": "/avatar-picker/static/js/runtime-main.778d2610.js"
+  }
+]);
